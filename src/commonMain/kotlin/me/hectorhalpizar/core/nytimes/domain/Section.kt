22362cafb58fbdbef3a08b05d4db48e5c71f22d8
Hector Alpizar <hector.alpizar.c@gmail.com>
2022-03-24 22:32:43 -0700

    Section list added

---
+++ src/commonMain/kotlin/me/hectorhalpizar/core/nytimes/domain/Section.kt
@@ -0,0 +1,32 @@
+package me.hectorhalpizar.core.nytimes.domain
+
+/**
+ *  List of Sections available
+ */
+enum class Section(val sectionName: String) {
+    ARTS("arts"),
+    AUTOMOBILES("automobiles"),
+    BOOKS("books"),
+    BUSINESS("business"),
+    FASHION(""),
+    FOOD("food"),
+    HEALTH("health"),
+    HOME("home"),
+    INSIDER("insider"),
+    MAGAZINE("magazine"),
+    MOVIES("movies"),
+    OBITUARIES("obituaries"),
+    OPINION("opinion"),
+    POLITICS("politics"),
+    REAL_ESTATE("realstate"),
+    SCIENCE("science"),
+    SPORTS("sport"),
+    SUNDAY_REVIEW("sundayreview"),
+    TECHNOLOGY("technology"),
+    THEATER("theater"),
+    T_MAGAZINE("t-magazine"),
+    TRAVEL("travel"),
+    UPSHOT("upshot"),
+    US("us"),
+    WORLD("world")
+}
